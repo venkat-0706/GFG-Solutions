22cb655768007437c5adf6070ad3af42417ea546
Abbireddy Venkata Chandu <119731968+venkat-0706@users.noreply.github.com>
2024-10-23 05:43:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find the Sum of Last N nodes of the Linked List/find-the-sum-of-last-n-nodes-of-the-linked-list.py
@@ -0,0 +1,59 @@
+#User function Template for python3
+
+'''
+# Node Class
+class Node:
+    def __init__(self, data):   # data -> value stored in node
+        self.data = data
+        self.next = None
+'''
+class Solution:
+    def sumOfLastN_Nodes(self, head, n):
+        count=0
+        temp=head
+        while (temp is not None):
+            count+=1
+            temp=temp.next
+            
+        k=count-n
+        temp=head
+        ans=0
+        while(k>0):
+            temp=temp.next
+            k-=1
+        while(temp is not None):
+            ans+=temp.data
+            temp=temp.next
+        return ans
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+if __name__ == "__main__":
+    import sys
+    input = sys.stdin.read
+    data = input().split('\n')
+
+    t = int(data[0])
+    for i in range(1, t + 1):
+        arr = list(map(int, data[2 * i - 1].split()))
+        n = int(data[2 * i])
+        head = Node(arr[0])
+        tail = head
+        for value in arr[1:]:
+            tail.next = Node(value)
+            tail = tail.next
+        ob = Solution()
+        print(ob.sumOfLastN_Nodes(head, n))
+
+# } Driver Code Ends
