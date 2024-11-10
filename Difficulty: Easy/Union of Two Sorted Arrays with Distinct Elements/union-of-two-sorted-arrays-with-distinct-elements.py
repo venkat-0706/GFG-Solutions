787976b0b222088fd0dede3fd2c5102d42b8dfd7
Abbireddy Venkata Chandu <119731968+venkat-0706@users.noreply.github.com>
2024-11-10 06:57:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Union of Two Sorted Arrays with Distinct Elements/union-of-two-sorted-arrays-with-distinct-elements.py
@@ -0,0 +1,26 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+#User function Template for python3
+class Solution:
+    #Function to return a list containing the union of the two arrays.
+    def findUnion(self,a,b):
+        return sorted(set(a+b))
+        # code here 
+
+#{ 
+ # Driver Code Starts.
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases):
+        a = list(map(int, input().strip().split()))
+        b = list(map(int, input().strip().split()))
+        ob = Solution()
+        li = ob.findUnion(a, b)
+        for val in li:
+            print(val, end=' ')
+        print()
+        print("~")
+# } Driver Code Ends
