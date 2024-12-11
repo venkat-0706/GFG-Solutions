9a92c67804407a3461a8dd6034a177b46aa4d1ae
Abbireddy Venkata Chandu <119731968+venkat-0706@users.noreply.github.com>
2024-12-12 05:18:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Number of occurrence/number-of-occurrence.py
@@ -0,0 +1,27 @@
+from collections import Counter
+class Solution:
+    def countFreq(self, arr, target):
+        return arr.count(target)
+        
+        
+        #code here
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+import bisect
+#Main
+if __name__ == '__main__':
+    t = int(input())
+    while t:
+        t -= 1
+        A = [int(x) for x in input().strip().split()]
+        nd = [int(x) for x in input().strip().split()]
+        D = nd[0]
+        ob = Solution()
+        ans = ob.countFreq(A, D)
+        print(ans)
+        print("~")
+
+# } Driver Code Ends
